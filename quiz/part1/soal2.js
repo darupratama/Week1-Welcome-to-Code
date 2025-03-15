6683bf3dfd7516d450bd9feae4a7bd1513ba9a19
darupratama <darupratama46@gmail.com>
2025-03-15 17:00:17 +0700

    submit tugas pertama

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,16 @@
+//tips gunakan keyword ini di google "conditional switch case javascript"
+//dan mulailah membaca dan mencontek di stackoverflow xixixi
+
+let tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+let bulan = 2; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+let tahun = 2001; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch (bulan){
+    case 1:
+        namabulan = "januari";
+        break;
+    case 2:
+            namabulan = "febuari";
+            break;        
+}
+console.log ( tanggal + "" + bulan+ "" +tahun);
