db4ce2a1891de79780d2fcadc78484a02bc3a871
darupratama <darupratama46@gmail.com>
2025-03-26 08:41:23 +0700

    tugas 2 done sir

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,10 @@
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word = 'wow JavaScript is so cool';
+
+let exampleFirstWord = word[0] + word[1] + word[2];;
+//jawaban
+let exampleFirsWord = word.split('')[0];
+console.log('first word: '+exampleFirstWord);
