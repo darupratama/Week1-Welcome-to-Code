8f0f67572ec0f91a3c391d8b3b53638898dcdec0
darupratama <darupratama46@gmail.com>
2025-03-26 12:51:14 +0700

    tugas ke part 2 done sir

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,13 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+// jawaban
+let kata = 'wow js is the best heheheh';
+let contoh = kata.substring(0,3);
+let length = contoh.length
+console.log ('kata pertama nya adalah: '+ contoh + ' yaps benar js is the best' + length)
