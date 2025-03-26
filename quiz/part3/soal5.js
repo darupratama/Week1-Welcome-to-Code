a2988fbe5ac5e1eddbe4cdb9ab584d8ba3df9e85
darupratama <darupratama46@gmail.com>
2025-03-26 13:21:51 +0700

    done juga sir, sorry sir kalo agak lambat hehhe

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,13 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+//jawaban nya sir disini 
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    console.log("*".repeat(i));
+}
