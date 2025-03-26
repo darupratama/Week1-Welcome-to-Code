9dc336cc12beba4730cc874816ca79fcadf2dfdd
darupratama <darupratama46@gmail.com>
2025-03-26 12:46:55 +0700

    done sir

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+
+//jawaban
+let kata = 'lingang guling guling';
+let coba = kata.substring(0,2);
+
+console.log('lingang guling guling' + coba)
