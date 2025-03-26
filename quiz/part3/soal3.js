36b041b11745687d293fbea85199cc5f4a201644
darupratama <darupratama46@gmail.com>
2025-03-26 13:16:47 +0700

    tugas ke 3 done sir

---
+++ quiz/part3/soal3.js
@@ -0,0 +1,15 @@
+
+// Problem
+// Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+// Di dalam perulangan, periksa setiap angka counter:
+
+// Apabila angka counter adalah angka genap, tuliskan GENAP
+// Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+//jawaban
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " - GENAP");
+    } else {
+        console.log(i + " - GANJIL");
+    }
+}
